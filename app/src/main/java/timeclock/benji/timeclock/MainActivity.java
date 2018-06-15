7604e498db0f1cb3385254cc864184026f9bd821
Benjamin <buriart2@asu.edu>
2018-06-14 20:42:22 -0700

    got the current time to show upon button press

--- app/src/main/java/timeclock/benji/timeclock/MainActivity.java
+++ app/src/main/java/timeclock/benji/timeclock/MainActivity.java
@@ -1,13 +1,44 @@
 package timeclock.benji.timeclock;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (EditText) findViewById(R.id.textView);
     }
+
+    public void clockIn(View view) {
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        this.text = new EditText(this);
+        text.setText(currentDateTimeString);
+      //  placeName.setHint("Name of Place");
+        layout.addView(text);
+        dialog.setView(layout);
+
+        dialog.show();
+
+    }
+
+
+
 }
