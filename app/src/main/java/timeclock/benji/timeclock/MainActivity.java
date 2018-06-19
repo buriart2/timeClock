239499b2eaaf44b3ff8f3cc7f5f37368f6f937dd
Benjamin <buriart2@asu.edu>
2018-06-19 16:50:47 -0700

    got the time to show on clock in and clock out. checkpoint 1 complete

--- app/src/main/java/timeclock/benji/timeclock/MainActivity.java
+++ app/src/main/java/timeclock/benji/timeclock/MainActivity.java
@@ -9,10 +9,14 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    EditText text;
+    EditText text, text2;
+    String clockintime;
+    String clockouttime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +27,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clockIn(View view) {
-        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+      //  String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("hh:mm:ss");
+        clockintime = mdformat.format(calendar.getTime());
 
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
 
         this.text = new EditText(this);
-        text.setText(currentDateTimeString);
+        text.setText(clockintime);
       //  placeName.setHint("Name of Place");
         layout.addView(text);
         dialog.setView(layout);
@@ -40,5 +47,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void clockOut(View view) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("hh:mm:ss");
+        clockouttime = mdformat.format(calendar.getTime());
+        //Double difference = Double.parseDouble(clockintime) - Double.parseDouble(clockouttime);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        //System.out.println("clock in as double is: " + Double.parseDouble(clockintime));
+        this.text2 = new EditText(this);
+        text2.setText(clockouttime);
+        //  placeName.setHint("Name of Place");
+        layout.addView(text2);
+        dialog.setView(layout);
 
+        dialog.show();
+    }
 }
